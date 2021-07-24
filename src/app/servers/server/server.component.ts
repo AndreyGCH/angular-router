15863e48d14aafb1db4e0d241ce91aa220eab01e
AndreyGCH <guevaraandrey@gmail.com>
2021-07-24 15:16:03 -0600

    router finished

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -14,14 +14,19 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService, private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id']
-    this.server= this.serversService.getServer(+id);
-    this.route.params.subscribe(
-      (params:Params) => {
-        this.server = this.serversService.getServer(+params['id']);
-        console.log('server',this.server, params['id'])
+    this.route.data.subscribe(
+      (data:Data) => {
+        this.server = data['server']
       }
-    )
+    );
+    // let id = this.route.snapshot.params['id']
+    // this.server= this.serversService.getServer(+id);
+    // this.route.params.subscribe(
+    //   (params:Params) => {
+    //     this.server = this.serversService.getServer(+params['id']);
+    //     console.log('server',this.server, params['id'])
+    //   }
+    // )
   }
 
   onEdit(){
